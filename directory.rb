b7253eb9eee1da6965b93ad58212deb2cd28a0e9
William Powell <william.powell92@gmail.com>
2018-04-05 07:48:23 +0100

    students sorted by cohort

--- directory.rb
+++ directory.rb
@@ -1,21 +1,77 @@
 # puts all student names into array
 def input_students
+  cohorts = [
+  :january,
+  :february,
+  :march,
+  :april,
+  :may,
+  :june,
+  :july,
+  :august,
+  :september,
+  :october,
+  :november,
+  :december,
+  :unknown
+  ]
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
+  puts "Please enter #{name}'s cohort"
+  cohort = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the students hash to the array
-    students << {name: name, cohort: :november, hobby: "world domination", country_of_birth: "hell", height: 183}
+    students << {name: name, cohort: cohort.to_sym, hobby: "world domination", country_of_birth: "hell", height: 183}
+
     puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
+    unless name.empty?
+      while true do
+        puts "Please enter #{name}'s cohort"
+        cohort = gets.chomp
+
+        if cohort == ""
+          cohort = :unknown
+        end
+
+        if cohorts.include?(cohort.to_sym)
+          break
+        end
+      end
+    end
   end
   # return the array of students
   students
+end
+
+def group_by_cohort(students)
+  cohorts = [
+  :january,
+  :february,
+  :march,
+  :april,
+  :may,
+  :june,
+  :july,
+  :august,
+  :september,
+  :october,
+  :november,
+  :december,
+  :unknown
+  ]
+
+  sorted_students = students.sort do |first, second|
+    cohorts.index(first[:cohort]) <=> cohorts.index(second[:cohort])
+  end
+
+  sorted_students
 end
 
 # prints the school header
@@ -40,10 +96,15 @@ end
 
 # prints the total number of students
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(50)
+  if students.count == 1
+    puts "Overall, we have #{students.count} great student".center(50)
+  else
+    puts "Overall, we have #{students.count} great students".center(50)
+  end
 end
 
 students = input_students
+grouped_students = group_by_cohort(students)
 print_header
-print(students)
+print(grouped_students)
 print_footer(students)
