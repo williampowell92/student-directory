a2ad94b3fb49d8396ee76187e33257c1e8b33aa6
William Powell <william.powell92@gmail.com>
2018-04-07 12:29:42 +0100

    Added add_to_students method

--- makers_directory.rb
+++ makers_directory.rb
@@ -1,5 +1,9 @@
 @students = [] # an empty array accessible to all methods
 
+def add_to_students(name, cohort = :november)
+  @students << {name: name, cohort: cohort}
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -8,7 +12,7 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    @students << {name: name, cohort: :november}
+    add_to_students(name)
     puts "Now we have #{@students.count} students"
     # get another name from the user
     name = STDIN.gets.chomp
@@ -77,7 +81,7 @@ def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+    add_to_students(name, cohort.to_sym)
   end
   file.close
 end
