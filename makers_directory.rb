a9829aa5a1d8c74ab08fd366bbd6d37f318e0610
William Powell <william.powell92@gmail.com>
2018-04-07 10:19:09 +0100

    Added save student data to csv functionality

--- makers_directory.rb
+++ makers_directory.rb
@@ -35,6 +35,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -50,11 +51,25 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit # this will cause the program to terminate
     else
       puts "I don't know what you meant, try again"
   end
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def interactive_menu
